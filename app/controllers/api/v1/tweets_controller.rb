e5c4bab954ca16586d410223ec5a10d93a7d3c9d
mahesh <maheshbabu@shipx.in>
2020-03-14 00:12:45 +0530

    profile and followers tweets actions added

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -2,11 +2,27 @@ class Api::V1::TweetsController < ApplicationController
   before_action :find_user
   
   def index
-      
+     render json: {tweets: user.tweets.order("created_at DESC"), code: 200}
   end
 
   def follow_and_unfollow
     
+  end
+
+  def follower_tweets
+    @follower = @user.followers.where(email: params[:follower_email]).first
+    @follower_tweets = @follower.try(:tweets).order("created_at DESC")
+    if @follower_tweets.present?
+      render json: {tweets: @follower_tweets, code: 200}
+    else
+      render json: {error: "follower doesn't have tweets yet", code: 200}
+    end
+  end
+
+  def profile
+    followers = @user.followers
+    following = @user.following
+    render json: {user: @user,email, followers: @followers, following: @following, code: 200} 
   end
 
   def find_user
